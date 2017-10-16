0a8a60edd0404f835b04bc923a429e325a941795
Helge Mjøs <31730850+HelgeMjos@users.noreply.github.com>
2017-10-16 09:13:08 +0200

    Update DoorStatus.ino

--- week2/DoorStatus/DoorStatus.ino
+++ week2/DoorStatus/DoorStatus.ino
@@ -9,8 +9,8 @@
 #define pin_switch1 D1
 #define pin_switch2 D2
 
-#define WLAN_SSID     "Torvmarkene_11"
-#define WLAN_PASSWORD "jegersjef1"
+#define WLAN_SSID     "xxxxxx"
+#define WLAN_PASSWORD "xxxxxx"
 
 DHT dht(DHTPIN, DHTTYPE, 11);
 
