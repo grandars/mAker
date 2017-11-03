6c840970769e0e33c3b8cc6773498d953db0dea3
Helge Mjøs <31730850+HelgeMjos@users.noreply.github.com>
2017-11-03 19:18:20 +0100

    Add files via upload

---
+++ week2/BMP280/BMP280.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+
+Adafruit_BMP280 bme; // I2C
+
+void setup() {
+    Wire.begin(2,0); // GPIO2, GPIOO -> D4, D3
+  Serial.begin(9600);
+  Serial.println(F("BMP280 test"));
+  
+  if (!bme.begin()) {  
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1);
+  }
+}
+
+void loop() {
+    Serial.print(F("Temperature = "));
+    Serial.print(bme.readTemperature());
+    Serial.println(" *C");
+    
+    Serial.print(F("Pressure = "));
+    Serial.print(bme.readPressure());
+    Serial.println(" Pa");
+
+    Serial.print(F("Approx altitude = "));
+    Serial.print(bme.readAltitude(1026.0)); // this should be adjusted to your local forcase
+    Serial.println(" m");
+    
+    Serial.println();
+    delay(2000);
+}
